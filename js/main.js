67c68e9e231cb39742b2edc3eaa310c93ac0cea2
rupeshkumar4455 <Kumarrupesh9211@gmail.com>
2026-04-18 13:32:59 +0530

    update - reduced the no. of predictions

--- js/main.js
+++ js/main.js
@@ -168,7 +168,7 @@ function analyzeSymptoms(inputText) {
   return scores
     .filter(d => d.matchCount > 0)
     .sort((a, b) => b.score - a.score)
-    .slice(0, 5);
+    .slice(0, 3);
 }
 
 function getSeverityClass(severity) {
